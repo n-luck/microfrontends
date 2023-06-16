6569187af3e62aeb7168b485bf26327ce6c8717b
Nadja Luckhaupt <nadja.luckhaupt@gmail.com>
2023-06-16 15:54:46 +0100

    add auth pages

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./app";
 
+// Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   const history =
     defaultHistory ||
@@ -27,11 +28,16 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   };
 };
 
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
+  const devRoot = document.querySelector("#_auth-dev-root");
+
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
+// We are running through container
+// and we should export the mount function
 export { mount };
